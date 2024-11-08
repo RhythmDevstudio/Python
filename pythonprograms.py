2f6757882b49b060c11674321e201e9a4ff879e3
RHYTHM MISHRA <125253220+RhythmDevstudio@users.noreply.github.com>
2024-11-08 07:54:26 +0530

    Add files via upload

---
+++ pythonprograms.py
@@ -0,0 +1,44 @@
+#  Print Hello World!
+# print("Hello World!")
+# print(35)
+
+# name= "Rhythm Mishra"
+# age=22
+# price= 25.99
+# age2=age
+
+# print("My Name is: ", name)
+# print("My Age is: ", age2)
+# print("The price is: ", price)
+
+# print(type(name))
+# print(type(age2))
+# print(type(price))
+
+
+# a= 10
+# b= 20
+
+# diff = a-b
+# sum= a+b
+# multiply= a*b
+# divide= a/b
+
+# print(diff)
+# print(sum)
+# print(multiply)
+# print(divide)
+
+# Printing input from the user 
+# name= input("Enter your name: ")
+# age= int(input("Enter your age: "))
+# price= float(input("Enter the price: "))    
+# print("Your name is: ", name)
+# print("Your age is: ", age)
+# print("The price is: ", price)
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# print("My name is:", name, "and my age is:", age, "and the price is:", price)
